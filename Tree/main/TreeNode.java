6082914c1280e927b941f69fb998a696d4e1740b
Prasad Deshpande <prasad.0210@gmail.com>
2016-05-21 18:04:21 -0700

    Create TreeNode.java

---
+++ Tree/main/TreeNode.java
@@ -0,0 +1,14 @@
+package main;
+
+public class TreeNode {
+
+	int data;
+	public TreeNode left;
+	public TreeNode right;
+	
+	public TreeNode(int data){
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
